cf6a9da1a93fb30bd7a1b888dec52c6a90e81aea
al <al@notearise.com>
2017-04-27 13:19:09 -0400

    CSS still trippin
    DAMN!

--- partials/header.php
+++ partials/header.php
@@ -7,12 +7,12 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
+    <link rel="icon" href="https://notearise.com/favicon.ico">
 
     <title>Thursday</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="<?= ACC_SCRIPTROOT ?>../../thursday/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= ACC_SCRIPTROOT ?>css/bootstrap.min.css" rel="stylesheet">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
