8baef0694336ecf5f1a0d3705bdd0eb7cfebedf4
al <al@notearise.com>
2017-04-27 15:33:24 -0400

    Added analytics & footer
    Added analytics and footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,15 @@
-    </div>
+<hr />
+<style>
+.copyright {padding-bottom: 50px;}
+</style>
+<div class="copyright">For testing purposes only, people! Don't be sharin' & shit.</p>&copy; <?php
+$copyYear = 2017; // Set your website start date
+$curYear = date('Y'); // Keeps the second year updated
+echo $copyYear . (($copyYear != $curYear) ? '-' . $curYear : '');
+?> Notearise, Inc. All rights reserved.</div>
+</div>
+
+<?php include_once("analyticstracking.php") ?>
+
   </body>
 </html>
