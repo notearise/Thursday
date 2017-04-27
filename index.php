a019d51aa6ea6d9051d8299a386afb2a696f7954
al <al@notearise.com>
2017-04-27 16:02:31 -0400

    Quick updates
    Quick updates

--- index.php
+++ index.php
@@ -13,10 +13,5 @@
 	<div class="jumbotron">
 		<h2>Welcome to <strong>Thursday</strong> <span class="glyphicon glyphicon-flash" aria-hidden="true"></span></h2>
 		<p>Let's take some notes!</p>
-	</div>For testing purposes only, people! Don't be sharin' & shit.</p>
-	&copy; <?php
-$copyYear = 2017; // Set your website start date
-$curYear = date('Y'); // Keeps the second year updated
-echo $copyYear . (($copyYear != $curYear) ? '-' . $curYear : '');
-?> Notearise, Inc. All rights reserved.
+	</div>
 <?php include "partials/footer.php" ?>
--- partials/footer.php
+++ partials/footer.php
@@ -2,7 +2,7 @@
 <style>
 .copyright {padding-bottom: 50px;}
 </style>
-<div class="copyright">For testing purposes only, people! Don't be sharin' & shit.</p>&copy; <?php
+<div class="copyright">For testing purposes only, people!</p>&copy; <?php
 $copyYear = 2017; // Set your website start date
 $curYear = date('Y'); // Keeps the second year updated
 echo $copyYear . (($copyYear != $curYear) ? '-' . $curYear : '');
