7ada6ae90cb3262c014cd9fe27741019d5189d7a
al <al@notearise.com>
2017-04-27 13:01:30 -0400

    DB Host Update
    Added IP address (205.186.128.172) rather than localhost since the app
    can run CI

--- control/db.php
+++ control/db.php
@@ -1,6 +1,6 @@
 <?php
 try{
-	$db = new PDO("mysql:host=localhost;dbname=thursday", "thursday" ,"B1@thering!");
+	$db = new PDO("mysql:host=205.186.128.172;dbname=thursday", "thursday" ,"B1@thering!");
 	$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 	$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 }catch(Exception $e){
